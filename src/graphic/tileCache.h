09e87c0438eef30b3e8315a0fcd5dc3ef487e385
oli4wolf <34458202+oli4wolf@users.noreply.github.com>
2024-02-24 22:12:00 +0100

    Formating.

--- src/graphic/tileCache.h
+++ src/graphic/tileCache.h
@@ -9,7 +9,7 @@
 
 // Define any global variables here
 const int TILE_SIZE = 256; // Downloaded Tilesizes from Swiss Topo.
-const int n_sprite_x = 3; // M5Core2 has a screen resolution of 320x240, so 3x3 tiles fit perfectly.
+const int n_sprite_x = 3;  // M5Core2 has a screen resolution of 320x240, so 3x3 tiles fit perfectly.
 const int n_sprite_y = 3;
 const int n_sprite = n_sprite_x * n_sprite_y;
 extern sprite_struct *tile_cache[n_sprite];
