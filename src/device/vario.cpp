d72252aa3ac152efe0a66347b7d7c679f727f685
oli4wolf <34458202+oli4wolf@users.noreply.github.com>
2024-05-30 20:12:15 +0200

    Calculation working. Screen info to optimize.

--- src/device/vario.cpp
+++ src/device/vario.cpp
@@ -3,16 +3,10 @@
 // Initialize Variables
 MS5637 barometricSensor;
 float last_pressure_reading = 0.0;
-int last_altitude_reading = 0;
-
-double climb_buffer[2 * CLIMB_SAMPLES]; // doubled the size of the array.
-uint8_t climb_buf_idx;
+float last_altitude = 0;
 
 /* climb rate in cm/s */
 int16_t climb_cms = 0; // Global
-
-// Let's calculate a window depending how fast the trigger is.
-int average_altitude = 0;
 
 KalmanFilter kalmanFilter = KalmanFilter();
 
@@ -26,22 +20,15 @@ void varioReadoutLoop(void *pvParameters)
     while (true)
     {
         last_pressure_reading = barometricSensor.getPressure();
-        ESP_LOGD("Climb", "Last Reading: %f", last_pressure_reading);
         if (pressureIsOk(last_pressure_reading))
         {
             kalmanFilter.update(last_pressure_reading);
 #ifdef __printVarioReadout__
-            ESP_LOGD("Climb", "Last Reading: %f, alt_cm: %d", last_pressure_reading, alt_cm);
+            ESP_LOGD("Climb", "Last Reading: %f", last_pressure_reading);
 #endif
 #ifdef __logclimb__
             logClimbAltMS(pressure);
 #endif
-            double filtered = kalmanFilter.get();
-            climb_buffer[climb_buf_idx] = filtered;
-            climb_buf_idx = climb_buf_idx + 1;
-            float altitude = 44330.0 * (1.0 - pow(filtered / filtered, 1.0 / 5.255));
-            ESP_LOGD("Climb", "Last_Reading: %f, Filtered: %f, altitude: %f, climb_buf_idx: %d", last_pressure_reading, filtered, altitude, climb_buf_idx);
-            climb_cms = (int16_t)altitude;
         }
         vTaskDelay(20 / portTICK_PERIOD_MS);
     }
@@ -52,27 +39,16 @@ void varioAverageLoop(void *pvParameters)
 {
     while (true)
     {
-        if (climb_buf_idx > 0)
-        {
-            // Calculate average.
-            for (int i = 0; i < climb_buf_idx; i++)
-            {
-                average_altitude = average_altitude+climb_buffer[i];
-            }
-            ESP_LOGD("Climb", "Average_altitude: %f, climb_buf_idx: %d", average_altitude, climb_buf_idx);
-            average_altitude = average_altitude/climb_buf_idx;
-            if (last_altitude_reading == 0){
-                last_altitude_reading = average_altitude;
-            }else if (climb_cms > 0){
-                climb_cms = (last_altitude_reading - average_altitude);
-            }
-            ESP_LOGD("Climb", "Climb: %d, Average_altitude: %f, last_altitude_reading: %d", climb_cms, average_altitude, last_altitude_reading);
-            last_altitude_reading = average_altitude;
-            climb_buf_idx = 0;
-            // tone(26, frequency[i], duration[i]);
-            // vTaskDelay(duty[i] * (duration[i]));
-            drawVarioInfo();
-        }
+        double filtered = kalmanFilter.get();
+        float altitude = 44330.0 * (1.0 - pow(filtered / 1013.25, 1.0 / 5.255));
+        ESP_LOGD("Climb", "last_altitude: %f, Filtered: %f, altitude: %f", last_altitude, filtered, altitude);
+        climb_cms = (int16_t)((altitude - last_altitude) * 100);
+        last_altitude = altitude;
+
+        // tone(26, frequency[i], duration[i]);
+        // vTaskDelay(duty[i] * (duration[i]));
+        drawVarioInfo();
+
         vTaskDelay(500);
     }
 }
